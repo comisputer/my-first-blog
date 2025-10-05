02c728b867ec51b6fd58dd8c9535ed5f8c78dbcd
inwoo <inwoo@gmail.com>
2025-10-06 02:43:56 +0900

    [+] add chart

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('chart/', views.chart_page, name='chart_page'),
+    path('chart/data/', views.chart_data, name='chart_data'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 # def post_list(request):
@@ -6,3 +7,28 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def chart_data(request):
+    """Return JSON data for a mixed bar + line chart."""
+    labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+    bar_values = [120, 180, 130, 160, 210, 190]
+    line_values = [100, 150, 140, 170, 200, 220]
+
+    return JsonResponse(
+        {
+            "labels": labels,
+            "bar": {
+                "name": "Sales",
+                "values": bar_values,
+            },
+            "line": {
+                "name": "Forecast",
+                "values": line_values,
+            },
+        }
+    )
+
+
+def chart_page(request):
+    return render(request, 'blog/chart.html')
